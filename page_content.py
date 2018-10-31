9511333b6ddc84c11b2c1961d7bd06eab0a03459
daniel sc <su7cheng@gmail.com>
2018-10-31 12:07:35 -0300

    Change to Bootstrap 4.

--- page_content.py
+++ page_content.py
@@ -6,17 +6,14 @@ main_page_head = '''
 	    <meta charset="utf-8">
 	    <title>Fresh Tomatoes!</title>
 
-	    <!-- Bootstrap 3 -->
-	    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-	    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
-	    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-	    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	    <!-- Bootstrap 4 -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
 
 	    <link rel="stylesheet" href="./styles/style.css">
 	    <script src="./scripts/script.js"></script>
-
 	</head>
-
 '''
 
 
